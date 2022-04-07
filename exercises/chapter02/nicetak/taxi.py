a8d3a058d7c19fa4442dd5d4c2fd33f084b94158
nicetak <kazuharu.yanagimoto@gmail.com>
2022-04-07 23:16:42 +0000

    2.4

--- exercises/chapter02/nicetak/taxi.py
+++ exercises/chapter02/nicetak/taxi.py
@@ -5,11 +5,21 @@ import numpy as np
 
 
 class Taxi:
-    def __init__(self, gamma=0.7, alpha=0.2, epsilon=0.1, n_episode=1000, method="Q-learning"):
+    def __init__(self):
 
         # Environment Setup
         self.env = gym.make("Taxi-v3")
         self.env.reset()
+
+        # Initialize Q
+        self.Q = np.zeros(
+            [self.env.observation_space.n, self.env.action_space.n])
+
+        # Logging
+        self.log_Q = []
+        self.log_reward = []
+
+    def train(self, method="Q-learning", verbose=False, gamma=0.7, alpha=0.2, epsilon=0.1, n_episode=1000):
 
         # Parameters
         self.gamma = gamma  # discount factor
@@ -18,26 +28,15 @@ class Taxi:
 
         self.n_episode = n_episode
 
-        # Initialize Q
-        self.Q = np.zeros(
-            [self.env.observation_space.n, self.env.action_space.n])
-
-        # Method "Q-learing" or "SARSA"
-        self.method = method
-
-        # Logging
-        self.log_Q = []
-        self.log_reward = []
-
-    def train(self, verbose=False):
-
         for episode in range(self.n_episode):
-            if self.method == "Q-learning":
+            if method == "Q-learning":
                 self.Q_one_episode()
+            elif method == "SARSA":
+                self.SARSA_one_episode()
             else:
                 raise ValueError("self.method should be 'Q-learning'")
 
-            if (episode % 100 == 0) and verbose:
+            if (episode % (self.n_episode // 10) == 0) and verbose:
                 print(f"Episode {episode} Total Reward: {self.log_reward[-1]}")
 
     def Q_one_episode(self):
@@ -60,6 +59,37 @@ class Taxi:
             self.Q[state, action] = new_value
             total_reward += reward
             state = next_state
+
+        self.log_Q.append(self.Q)
+        self.log_reward.append(total_reward)
+
+    def SARSA_one_episode(self):
+        done = False
+        total_reward = 0
+        current_state = self.env.reset()
+        if random.uniform(0, 1) < self.epsilon:
+            current_action = self.env.action_space.sample()  # Explore state space
+        else:
+            # Exploit learned values
+            current_action = np.argmax(self.Q[current_state])
+        while not done:
+            next_state, reward, done, info = self.env.step(
+                current_action)  # invoke Gym
+            if random.uniform(0, 1) < self.epsilon:
+                next_action = self.env.action_space.sample()  # Explore state space
+            else:
+                # Exploit learned values
+                next_action = np.argmax(self.Q[next_state])
+            sarsa_value = self.Q[next_state, next_action]
+            old_value = self.Q[current_state, current_action]
+
+            new_value = old_value + self.alpha * \
+                (reward + self.gamma * sarsa_value - old_value)
+
+            self.Q[current_state, current_action] = new_value
+            total_reward += reward
+            current_state = next_state
+            current_action = next_action
 
         self.log_Q.append(self.Q)
         self.log_reward.append(total_reward)
