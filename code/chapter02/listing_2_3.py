7e780b34187852c6319dcf464f31406d8653fc4c
Yuya Furusawa <fullyou0798@gmail.com>
2022-04-09 16:49:52 +0900

    教科書のコードを修正

--- code/chapter02/listing_2_3.py
+++ code/chapter02/listing_2_3.py
@@ -1,21 +1,35 @@
 import gym
 import numpy as np
 
+"""
+`env.nS`や`env.nA`が使われていたが、Taxi-v3にそのようなattributeは存在しない
+そのため、以下のように書き換えを行った
+
+env.nS -> env.observation_space.n
+env.nA -> env.action_space.n
+"""
+
 def iterate_value_function(v_inp, gamma, env):
-    ret = np.zeros(env.nS)
-    for sid in range(env.nS):
-        temp_v = np.zeros(env.nA)
-        for action in range(env.nA):
+    num_states = env.observation_space.n
+    num_actions = env.action_space.n
+
+    ret = np.zeros(num_states)
+    for sid in range(num_states):
+        temp_v = np.zeros(num_actions)
+        for action in range(num_actions):
             for (prob, dst_state, reward, is_final) in env.P[sid][action]:
                 temp_v[action] += prob*(reward + gamma*v_inp[dst_state]*(not is_final))
         ret[sid] = max(temp_v)
     return ret
 
 def build_greedy_policy(v_inp, gamma, env):
-    new_policy = np.zeros(env.nS)
-    for state_id in range(env.nS):
-        profits = np.zeros(env.nA)
-        for action in range(env.nA):
+    num_states = env.observation_space.n
+    num_actions = env.action_space.n
+
+    new_policy = np.zeros(num_states)
+    for state_id in range(num_states):
+        profits = np.zeros(num_actions)
+        for action in range(num_actions):
             for (prob, dst_state, reward, is_final) in env.P[state_id][action]:
                 profits[action] += prob*(reward + gamma*v_inp[dst_state])  # v[dst_state] は v_inp のタイポ?
         new_policy[state_id] = np.argmax(profits)
@@ -26,10 +40,12 @@ gamma = 0.9
 cum_reward = 0
 n_rounds = 500
 env.reset()
+num_states = env.observation_space.n
+
 for t_rounds in range(n_rounds):
     # init env and value function
     observation = env.reset()
-    v = np.zeros(env.nS)
+    v = np.zeros(num_states)
 
     # solve MDP
     for _ in range(100):
